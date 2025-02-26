5920c8feee764c0322c2a4ad4eb8bd8b92ed229f
Rouzihiro <ryossj@gmail.com>
2025-02-26 16:26:00 +0100

    first

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A collection of assets (wallpapers, avatars, lockscreen)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.assets = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+      name = "assets";
+      src = ./.;
+      installPhase = ''
+        mkdir -p $out
+        cp -r $src/wallpapers $out/
+        cp -r $src/wallpapers-live $out/
+        cp -r $src/avatars $out/
+        cp -r $src/lockscreen $out/
+        cp -r $src/icons $out/
+      '';
+    };
+  };
+}
