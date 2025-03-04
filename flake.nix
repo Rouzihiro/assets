0782b4fcf5f8544a8eeafe8a6e327d74e5af8216
Rouzihiro <ryossj@gmail.com>
2025-03-04 14:44:49 +0100

    update

--- flake.nix
+++ flake.nix
@@ -5,13 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages.x86_64-linux.assets = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
       name = "assets";
       src = ./.;
       installPhase = ''
         mkdir -p $out
         cp -r $src/wallpapers $out/
+        cp -r $src/wallpapers-live $out/
         cp -r $src/avatars $out/
         cp -r $src/lockscreen $out/
         cp -r $src/icons $out/
